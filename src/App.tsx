adbcde3a9a00b1a3e987c7e198c939e544c1395c
AIY7788 <aiykhounnakhon@gmail.com>
2026-02-26 20:14:11 +0700

    Refactor App component to remove unused code and implement main layout with header, input form, item lists and add style

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import FilterBtnGroup from './components/FilterBtnGroup';
+import FormInput from './components/FormInput';
+import Header from './components/Header';
+import ItemLists from './components/ItemLists';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen sm:flex sm:items-center font-sans font-normal text-base py-8 px-4 text-navy-850 bg-gray-300 dark:text-purple-100 dark:bg-navy-950 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-contain bg-top sm:bg-[url('./assets/images/bg-desktop-light.jpg')] dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] sm:dark:bg-[url('./assets/images/bg-desktop-dark.jpg')] transition-all duration-300">
+      <div className="mx-auto sm:min-w-lg">
+        <Header />
+        <main className="mt-8 flex flex-col gap-4 sm:gap-6 sm:mt-12">
+          <FormInput />
+          <div className="bg-gray-50 dark:bg-navy-900 rounded-md">
+            <ItemLists />
+
+            <div className="flex items-center justify-between gap-3 py-2.5 px-5 text-gray-600 dark:text-purple-700 text-sm">
+              <p>5 items left</p>
+              <div className="hidden font-bold sm:flex sm:items-center sm:gap-4">
+                <FilterBtnGroup />
+              </div>
+              <button className="cursor-pointer hover:text-navy-850 dark:hover:text-purple-100 transition-colors duration-300">
+                Clear Completed
+              </button>
+            </div>
+          </div>
+          <div className="bg-gray-50 text-gray-600 dark:bg-navy-900 dark:text-purple-700 py-4 px-5 rounded-md text-sm flex items-center gap-4 justify-center sm:hidden font-bold">
+            <FilterBtnGroup />
+          </div>
+        </main>
+
+        <div className="text-gray-600 dark:text-purple-600 text-sm text-center mt-8">
+          Drag and drop to reorder list
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
