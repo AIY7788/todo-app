66be753299baaaee960b614cd3e1b13679b029eb
AIY7788 <aiykhounnakhon@gmail.com>
2026-02-27 17:12:54 +0700

    Refactor context management by replacing FiltersProvider with ContextsProvider in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { ThemeProvider, FiltersProvider } from './contexts/Context.tsx';
+import { ThemeProvider } from './contexts/ThemeProvider.tsx';
+import { ContextsProvider } from './contexts/ContextProvider.tsx';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <FiltersProvider>
+      <ContextsProvider>
         <App />
-      </FiltersProvider>
+      </ContextsProvider>
     </ThemeProvider>
   </StrictMode>,
 );
