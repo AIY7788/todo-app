53ac8ac506cd8315bd08c7f347b67786a57b8b45
AIY7788 <aiykhounnakhon@gmail.com>
2026-02-28 09:06:38 +0700

    Update todoDB structure to align with Todo type and correct item properties

--- src/data/todoLists.ts
+++ src/data/todoLists.ts
@@ -1,10 +1,10 @@
 import type { Todo } from "../types/data.type";
 
-export const todoDB: Todo = [
-  { id: 1, title: "Learn React", isActive: true, completed: false },
-  { id: 2, title: "Write Blog", isActive: false, completed: true },
-  { id: 3, title: "Read Book", isActive: true, completed: false },
-  { id: 4, title: "Learn Backend", isActive: true, completed: false },
-  { id: 5, title: "Create Project", isActive: false, completed: false },
-  { id: 6, title: "Learn English", isActive: false, completed: true },
+export const todoDB: Todo[] = [
+  { id: "1", title: "Learn React", completed: false },
+  { id: "2", title: "Master TypeScript", completed: false },
+  { id: "3", title: "Build a Todo App", completed: true },
+  { id: "4", title: "Understand UseState", completed: true },
+  { id: "5", title: "Fetch API Data", completed: false },
+  { id: "6", title: "Deploy to Vercel", completed: false },
 ];
